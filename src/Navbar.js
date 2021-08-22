22b21adbf4461ebcc814fec8c77b5a1c292820f6
Rajib Mondal <mondal021002rajib@gmail.com>
2021-08-22 23:02:27 +0530

    Styling the navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,31 +4,31 @@ function Navbar() {
     const [show, setShow] = useState(false);
     return (
         <>
-            <section classNameName="navbar-bg">
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <section className="navbar-bg">
+                <nav className="navbar navbar-expand-lg navbar-light">
                     <div className="container">
-                        <a className="navbar-brand" href="#">Geeky Pay</a>
+                        <a className="navbar-brand" href="/">Geeky Pay</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={() => setShow(!show)} >
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className={`collapse navbar-collapse ${show ? "show" : ""}`} >
                             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Services</a>
+                                    <a className="nav-link" href="/">Services</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">About</a>
+                                    <a className="nav-link" href="/">About</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Contact</a>
+                                    <a className="nav-link" href="/">Contact</a>
                                 </li>
                             </ul>
                             <form className="d-flex">
-                                <button className="btn btn-outline-success" type="submit">Sign Up</button>
-                                <button className="btn btn-outline-success" type="submit">Login</button>
+                                <button className="btn btn-style" type="submit">Sign Up</button>
+                                <button className="btn btn-style btn-style-border" type="submit">Login</button>
                             </form>
                         </div>
                     </div>
